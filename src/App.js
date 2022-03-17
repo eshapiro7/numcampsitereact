4521bd54bc914e9333d06078796d11febe47a2db
Elizabeth Shapiro <elizabethashapiro@gmail.com>
2022-03-17 18:28:37 -0400

    controlled forms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
 
+const store = ConfigureStore();
 class App extends Component {
   render() {
       return (
-        <Router>
-            <div className="App">
-                <Main/>
-            </div>
-        </Router>
+        <Provider store={store}>
+          <Router>
+              <div className="App">
+                  <Main/>
+              </div>
+          </Router>
+        </Provider>
       );
   }
 }
